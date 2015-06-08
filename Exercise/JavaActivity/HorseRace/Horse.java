8d6dfc55d4c4143dd7e7cfbab32129419fd7561c
Rogelee <rogelee.bandioque@gmail.com>
2015-06-08 16:21:29 +0800

    HorseRace

--- Exercise/JavaActivity/HorseRace/Horse.java
+++ Exercise/JavaActivity/HorseRace/Horse.java
@@ -1,15 +1,15 @@
 public class Horse extends Thread{
-	//public static Integer horsesIn = 0;
-	static Message mes = new Message();
 	private String sound;
 	private int delay;
 	private int time;
 	private int distance;
+	private Message mes;
 	
-	public Horse(String n, String s, Integer t, int dis){
+	public Horse(String n, String s, Integer t, int dis,Message mes){
 		sound = s;
 		time = t;
 		distance = dis;
+		this.mes = mes;
 		this.setName(n);
 	}
 
@@ -19,55 +19,30 @@ public class Horse extends Thread{
 
 	public void run(){
 		try{
-			Horse.sleep(time*1000);		
-			System.out.println(getName()+" is ready to start and has traveled "+ time + " second/s");
-			mes.count++;			
-			synchronized(mes){
+			mes.count++;
+			System.out.println(getName()+" is ready to start and has traveled "+ time + " second/s " + mes.count );				
+			synchronized(mes){				
 				mes.wait();
-				/*if(mes.count !=5){
-					//System.out.println(getName() + " is waiting");
-					mes.wait();	
-				}else{
-					Horse.sleep(1000);
-					System.out.println("\n<ALL HORSES IN STARTING LINE>\n");
-					for(int i=3; i>=1; i--){
-						System.out.print(i+"!");
-						Horse.sleep(1000);
-					}
-					System.out.println("GO\n");
-					Horse.sleep(1000);
-					mes.notifyAll();
-				}*/
-			}
-			
-			System.out.println(getName() + " Started at:" +System.currentTimeMillis() + "\n");		
-			go();
-		}catch(InterruptedException e){
+				System.out.println(getName() + " Started at:" +System.currentTimeMillis() + "\n");		
+				go();
+			}		
+		} catch(InterruptedException e){
 			e.printStackTrace();
 		}
 	}
 
 	public void go(){
-		
-		try{
-			//Horse.sleep(1000);	
-			int toTravel = distance;			
-			do{
-				int travel = (int)(Math.random()*10)+1;
-				if(toTravel<travel){
-					travel = toTravel; 
-				}
-				Horse.sleep(travel*1000);	
-				toTravel = toTravel - travel;	
-				System.out.println(getName() + " ran "+ travel +"m and have to travel " + toTravel + "m more");	
+		int toTravel = distance;			
+		do {
+			int travel = (int)(Math.random()*10)+1;
+			if(toTravel<travel){
+				travel = toTravel; 
+			}
+			toTravel = toTravel - travel;	
+			System.out.println(getName() + " ran "+ travel +"m and have to travel " + toTravel + "m more");	
 
-			}while(toTravel>0);
-			//Horse.sleep(500);
-			System.out.println("\n<<"+ getName() + " finished the race! : " + getSound()+">>\n");
-		}catch(InterruptedException e){
-			e.printStackTrace();
-		}
-		
+		} while(toTravel>0);
+		System.out.println("\n<<"+ getName() + " finished the race! : " + getSound()+">>\n");
 	}
 
 	
--- Exercise/JavaActivity/HorseRace/HorseRace.java
+++ Exercise/JavaActivity/HorseRace/HorseRace.java
@@ -1,7 +1,9 @@
 import java.util.*;
 
 public class HorseRace{
+	
 	public static void main(String[]args){
+		Message mes = new Message();
 		int distance;
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter Distance (30 or greater): ");
@@ -14,15 +16,22 @@ public class HorseRace{
 		ArrayList<Integer> time = new ArrayList<Integer>();
 		for(int d=0; d<5; d++){
 			time.add((int)(Math.random()*10)+1);
-			//time.add(8);
 		}
-		int max = Collections.max(time);
-		Horse h1 = new Horse("Red Horse","grrr",time.get(0),distance);
-		Horse h2 = new Horse("Mucho Horse","boomboom",time.get(1),distance);
-		Horse h3 = new Horse("Sea Horse","plop plop",time.get(2),distance);
-		Horse h4 = new Horse("Beki Horse","keeeriii",time.get(3),distance);
-		Horse h5 = new Horse("Petrang Horse","mehehehe",time.get(4),distance);
-		Alert alert = new Alert();
+		
+		List<Horse> horses = new ArrayList<Horse>();
+		Horse h1 = new Horse("Red Horse","grrr",time.get(0),distance,mes);
+		Horse h2 = new Horse("Mucho Horse","boomboom",time.get(1),distance,mes);
+		Horse h3 = new Horse("Sea Horse","plop plop",time.get(2),distance,mes);
+		Horse h4 = new Horse("Beki Horse","keeeriii",time.get(3),distance,mes);
+		Horse h5 = new Horse("Petrang Horse","mehehehe",time.get(4),distance,mes);
+		
+		horses.add(h1);
+		horses.add(h2);
+		horses.add(h3);
+		horses.add(h4);
+		horses.add(h5);
+
+		Alert alert = new Alert(mes, horses);
 		
 		alert.start();
 		h1.start();
